7e0f370cb1910631f93ae5b3481aeb8ff884085c
MinchenZKN <minchen.zhu@uni-konstanz.de>
2017-12-03 19:50:05 +0100

    blahblah

--- exercises_02/ex02/ex04/ex04_otherLayer.py
+++ exercises_02/ex02/ex04/ex04_otherLayer.py
@@ -3,6 +3,7 @@ import os
 import tensorflow as tf
 import sys
 import code
+import matplotlib.pyplot as plt
 os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 os.environ['CUDA_VISIBLE_DEVICES']='0'
 
@@ -52,28 +53,35 @@ def max_pool_2x2(x):
                         strides=[1, 2, 2, 1], padding='SAME')
 
 
+def showImg(x):
+  img = x.eval()
+  for i in range(img.shape[3]):
+    ax = plt.subplot(4, 8, i+1)
+    ax.imshow(img[0,:,:,i])
+  plt.show()
+
+
 data = prep(50)
 
 
-x = tf.placeholder(tf.float32, shape=[None, 28, 28])
-y_ = tf.placeholder(tf.int64, shape=[None, 10])
-# y_onehot = tf.one_hot(y_, 10)
+init = tf.constant(np.random.rand(1, 14, 14, 32), dtype=tf.float32)
+x = tf.get_variable("x", initializer=init)
+x_image = tf.reshape(x, [1, 14, 14, 32])
 
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-
-
+'''
 # 1st convolutional layer
 W_conv1 = weight_variable("W_conv1", [5, 5, 1, 32])
 b_conv1 = bias_variable("b_conv1", [32])
 
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
+'''
 
 # 2nd convolutional layer
 W_conv2 = weight_variable("W_conv2", [5, 5, 32, 64])
 b_conv2 = bias_variable("b_conv2", [64])
 
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_conv2 = tf.nn.relu(conv2d(x, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
 # densely connected layer
@@ -95,30 +103,20 @@ y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
 
 
-saver = tf.train.Saver()
+
+lam = 0.5
+cls = 2
+
+saver = tf.train.Saver(var_list=[W_conv2,b_conv2,W_fc1,b_fc1,W_fc2,b_fc2])
 # training
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+optResp = lam * tf.reduce_sum(x * x) - tf.gather(tf.transpose(y_conv), cls)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(optResp, var_list=[x])
 
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
-  for i in range(100):
-    # batch = mnist.train.next_batch(50)
-    ((X,Y_), data) = next_batch(data)
-    if i % 100 == 0:
-      train_accuracy = accuracy.eval(feed_dict={
-          x: X, y_: Y_, keep_prob: 1.0})
-      print('step %d, training accuracy %g' % (i, train_accuracy))
-    train_step.run(feed_dict={x: X, y_: Y_, keep_prob: 0.5})
+  saver.restore(sess, "./model.ckpt")
+  for i in range(100000):
+    train_step.run(feed_dict={keep_prob: 0.5})
 
-  (tX,tY_) = test_data(data)
-  print('test accuracy %g' % accuracy.eval(feed_dict={
-      x: tX, y_: tY_, keep_prob: 1.0}))
-
-
-  save_path = saver.save(sess, "./model.ckpt")
-  code.interact(local=locals())
-
+  showImg(x)
+  #code.interact(local=locals())
